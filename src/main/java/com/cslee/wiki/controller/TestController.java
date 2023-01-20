79f08b607455156aaea21450ec736b1756c7e69d
cslee <cslee@stu.xidian.edu.cn>
2023-01-20 23:15:51 +0800

    2-6 开发Hello World接口 访问地址：http://127.0.0.1:8880/hello

---
+++ src/main/java/com/cslee/wiki/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.cslee.wiki.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, World";
+    }
+}
