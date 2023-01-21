be24ced70161228c3d411106d09fec299b00d9b0
cslee <cslee@stu.xidian.edu.cn>
2023-01-21 11:22:34 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/cslee/wiki/controller/TestController.java
+++ src/main/java/com/cslee/wiki/controller/TestController.java
@@ -2,6 +2,7 @@ package com.cslee.wiki.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello, World";
+        return "Hello, World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello, World!" + name;
     }
 }
