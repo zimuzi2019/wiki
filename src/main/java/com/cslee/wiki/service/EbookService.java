036f112a6bf921ed22d82baaea5df9f3b0bb69db
cslee <cslee@stu.xidian.edu.cn>
2023-01-22 23:14:59 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/cslee/wiki/service/EbookService.java
+++ src/main/java/com/cslee/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.cslee.wiki.domain.EbookExample;
 import com.cslee.wiki.mapper.EbookMapper;
 import com.cslee.wiki.req.EbookReq;
 import com.cslee.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.cslee.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,12 +23,16 @@ public class EbookService {
         criteria.andNameLike("%" + ebookReq.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+        //List<EbookResp> respList = new ArrayList<>();
+        //for (Ebook ebook : ebookList) {
+        //    //EbookResp ebookResp = new EbookResp();
+        //    //BeanUtils.copyProperties(ebook, ebookResp);
+        //    //对象复制
+        //    EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //    respList.add(ebookResp);
+        //}
+
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
 
         return respList;
     }
