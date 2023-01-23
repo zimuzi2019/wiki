3ce5cd962b80fd650200683a37f5ff2c1dbe990f
cslee <cslee@stu.xidian.edu.cn>
2023-01-23 19:04:26 +0800

    4-7  制作the-footer组件，去掉HelloWorld组件相关代码

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <a-layout-footer style="text-align: center">
+      Simple Wiki Project @2023 Created by cslee
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
