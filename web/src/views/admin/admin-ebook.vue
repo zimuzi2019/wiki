54206505e99c8c87cdc21b925a4ee31940808540
cslee <cslee@stu.xidian.edu.cn>
2023-01-25 18:36:50 +0800

    6-11 为保存接口增加参数校验

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -169,16 +169,18 @@ export default defineComponent({
       modalLoading.value = true;
 
       axios.post("/ebook/save", ebook.value).then((response) => {
+        modalLoading.value = false;
         const data = response.data;  // data = commonResp
         if (data.success) {
           modalVisible.value = false;
-          modalLoading.value = false;
 
           //重新加载列表
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize,
           });
+        } else {
+          message.error(data.message);
         }
       })
     };
