96b47881c47c831ea17a64bcdaecd8eb5add6ce2
cslee <cslee@stu.xidian.edu.cn>
2023-02-01 00:43:44 +0800

    8-3 从分类管理复制出文档管理的前后端代码 遗留问题：编辑表单中，只能选择一级文档作为父文档

---
+++ src/main/java/com/cslee/wiki/controller/DocController.java
@@ -0,0 +1,54 @@
+package com.cslee.wiki.controller;
+
+
+import com.cslee.wiki.req.DocQueryReq;
+import com.cslee.wiki.req.DocSaveReq;
+import com.cslee.wiki.resp.DocQueryResp;
+import com.cslee.wiki.resp.CommonResp;
+import com.cslee.wiki.resp.PageResp;
+import com.cslee.wiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp> > resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq docReq) {
+        CommonResp<PageResp<DocQueryResp> > resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(docReq);
+        resp.setSuccess(true);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq docReq) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(docReq);
+        return resp;
+    }
+
+    @DeleteMapping ("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+
+}
---
+++ src/main/java/com/cslee/wiki/service/DocService.java
@@ -0,0 +1,95 @@
+package com.cslee.wiki.service;
+
+import com.cslee.wiki.domain.Doc;
+import com.cslee.wiki.domain.DocExample;
+import com.cslee.wiki.mapper.DocMapper;
+import com.cslee.wiki.req.DocQueryReq;
+import com.cslee.wiki.req.DocSaveReq;
+import com.cslee.wiki.resp.DocQueryResp;
+import com.cslee.wiki.resp.PageResp;
+import com.cslee.wiki.util.CopyUtil;
+import com.cslee.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DocService {
+    private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
+
+    @Resource
+    private DocMapper docMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public List<DocQueryResp> all(){
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        //列表复制
+        List<DocQueryResp> respList = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        return respList;
+    }
+
+
+    public PageResp<DocQueryResp> list(DocQueryReq docReq){
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        DocExample.Criteria criteria = docExample.createCriteria();
+        PageHelper.startPage(docReq.getPage(), docReq.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        //List<DocResp> respList = new ArrayList<>();
+        //for (Doc doc : docList) {
+        //    //DocResp docResp = new DocResp();
+        //    //BeanUtils.copyProperties(doc, docResp);
+        //    //对象复制
+        //    DocResp docResp = CopyUtil.copy(doc, DocResp.class);
+        //    respList.add(docResp);
+        //}
+
+        //列表复制
+        List<DocQueryResp> respList = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        PageResp<DocQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(respList);
+
+        return pageResp;
+    }
+
+
+
+    /**
+     * 保存
+     */
+    public void save(DocSaveReq docSaveReq) {
+        Doc doc = CopyUtil.copy(docSaveReq, Doc.class);
+
+        if (ObjectUtils.isEmpty(docSaveReq.getId())) {
+            // 新增
+            doc.setId(snowFlake.nextId());
+            docMapper.insertSelective(doc);
+        } else {
+            // 更新
+            docMapper.updateByPrimaryKey(doc);
+        }
+    }
+
+    public void delete(Long id) {
+        docMapper.deleteByPrimaryKey(id);
+    }
+}
